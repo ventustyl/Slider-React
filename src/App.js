089decc93e8b8c1481cec6f10f7871a32c1b63e3
Eric Venturino <eric.venturino@orange.fr>
2022-09-07 07:12:31 +0200

    Premier envoi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Slider from './Components/Slider/Slider'
+
+
+function App() {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import './Slider.css'
+import BtnSlider from './BtnSlider'
+import dataSlider from './dataSlider'
+
+export default function Slider() {
+
+    const [slideIndex, setSlideIndex] = useState(1)
+
+    const nextSlide = () => {
+        if(slideIndex !== dataSlider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === dataSlider.length){
+            setSlideIndex(1)
+        }
+    }
+
+    const prevSlide = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(dataSlider.length)
+        }
+    }
+
+    const moveDot = index => {
+        setSlideIndex(index)
+    }
+
+    return (
+        <div className="container-slider">
+            {dataSlider.map((obj, index) => {
+                return (
+                    <div
+                    key={obj.id}
+                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        <img 
+                        src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="im"
+                        />
+                    </div>
+                )
+            })}
+            <BtnSlider moveSlide={nextSlide} direction={"next"} />
+            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
+
+            <div className="container-dots">
+                {Array.from({length: 5}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    )
+}
